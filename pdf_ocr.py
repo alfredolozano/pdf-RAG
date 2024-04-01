32bdfa8e3bf1b56d7b9750b3b9bde4fb51da3e4d
a <lozanoa94@gmail.com>
2024-04-01 11:22:10 -0600

    update yolo model location

--- pdf_ocr.py
+++ pdf_ocr.py
@@ -17,7 +17,7 @@ ocr_model = PaddleOCR(lang="es", use_gpu=True)  # 'es' for Spanish
 # load model
 device: str = "mps" if torch.backends.mps.is_available() else "cpu"
 
-table_model = YOLO('/content/drive/MyDrive/AFIRME - RAGente de Poliza/v3/runs/detect/train3/weights/best.pt')
+table_model = YOLO('/content/drive/MyDrive/AFIRME - RAGente de Poliza/v4_mult/detect/train3/weights/best.pt')
 table_model.to(device)
 
 # # set model parameters
